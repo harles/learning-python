d243fdecebd298007d341370460c1c27ac83b9e3
Harles Paesüld <harles.paesuld@bigbank.ee>
2017-03-20 23:09:23 +0200

    Add initial version of the test exercise

---
+++ programmeerimise_alused_II/nadal_0/n0.1.py
@@ -0,0 +1,16 @@
+# Kursuse kontrollülesanne
+# Allikas: https://courses.cs.ut.ee/2017/eprogalused2/spring/Main/Kontrollylesanne
+import unittest
+
+
+def parandatud_tulemus(vigane_tulemus_m, mõõteparandus_cm):
+    tegelik_tulemus = round(vigane_tulemus_m + mõõteparandus_cm/100, 2)
+    return tegelik_tulemus
+
+
+class MyTest(unittest.TestCase):
+    def test_tulemus_standardväärtused(self):
+        self.assertEqual(parandatud_tulemus(1.81, 20), 2.01)
+
+    def test_tulemus_üle_kahe_komakoha(self):
+        self.assertEqual(parandatud_tulemus(1.0012, 50), 1.50)
