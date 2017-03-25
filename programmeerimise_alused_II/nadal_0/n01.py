f36132c0b01fc55e384004c2b1b9eae9094871b5
Harles Paesüld <harles.paesuld@bigbank.ee>
2017-03-25 20:39:39 +0200

    Add full functionality

--- programmeerimise_alused_II/nadal_0/n01.py
+++ programmeerimise_alused_II/nadal_0/n01.py
@@ -1,5 +1,6 @@
 # Kursuse kontrollülesanne
 # Allikas: https://courses.cs.ut.ee/2017/eprogalused2/spring/Main/Kontrollylesanne
+# Näidis sisendfail: kaugus.txt
 import unittest
 
 
@@ -9,6 +10,28 @@ def parandatud_tulemus(vigane_tulemus_m, mõõteparandus_cm):
 
 print(parandatud_tulemus(1.1, 35))
 
+sisendfail_nimi = input("Sisestage failinimi: ")
+parandus_cm = int(input("Sisestage parandus sentimeetrites: "))
+normatiiv_m = float(input("Sisestage meistrivõistluste normatiiv meetrites: "))
+
+normatiivi_täitjad = 0
+normatiivi_täitjate_tulemuste_summa = 0
+
+sisendfail = open(sisendfail_nimi, encoding="UTF-8")
+
+print("Tegelikud tulemused")
+for tulemus in sisendfail:
+    tegelik_tulemus = parandatud_tulemus(float(tulemus), parandus_cm)
+    print(tegelik_tulemus)
+
+    if tegelik_tulemus >= normatiiv_m:
+        normatiivi_täitjad += 1
+        normatiivi_täitjate_tulemuste_summa += tegelik_tulemus
+
+
+if normatiivi_täitjad >= 1:
+    print("Normatiivi täitis",normatiivi_täitjad)
+    print("Täitnute keskmine",round(normatiivi_täitjate_tulemuste_summa/normatiivi_täitjad, 2))
 
 class MyTest(unittest.TestCase):
     def test_tulemus_standardväärtused(self):
