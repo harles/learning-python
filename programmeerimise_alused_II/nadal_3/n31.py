72c3e850eb17052c57327d4935db414e7069fba6
Harles Paesüld <harles.paesuld@bigbank.ee>
2017-04-20 22:12:03 +0300

    Add week 3 exercise 1

---
+++ programmeerimise_alused_II/nadal_3/n31.py
@@ -0,0 +1,13 @@
+# Kolmanda nädala 1. ülesanne
+# Allikas: https://moodle.ut.ee/mod/vpl/view.php?id=304040
+
+def symbolite_sagedus(sisend):
+    koond = {}
+
+    for tahemark in sisend:
+        if not tahemark in koond:
+            koond.update({tahemark : 1})
+        else:
+            koond.update({tahemark : koond[tahemark]+1})
+
+    return koond
