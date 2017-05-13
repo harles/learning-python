77bfbfe34597fece92b1554acc8e9d95fddc8165
Harles Paesüld <harles.paesuld@bigbank.ee>
2017-05-13 11:19:22 +0300

    Exercise 2 solution with one function parameter

--- programmeerimise_alused_II/nadal_6/n62.py
+++ programmeerimise_alused_II/nadal_6/n62.py
@@ -1,15 +1,18 @@
 # Kuuenda nädala 2. ülesanne
 # Allikas: https://moodle.ut.ee/mod/vpl/view.php?id=308759
 
-def kuup(jarj, i = 0, kuup_jarj=[]):
+def kuup(jarj):
 
-    if i < len(jarj):
+    kuup_jarj = []
 
-        kuup_jarj.append(jarj[i] ** 3)
+    if len(jarj) > 0:
 
-        kuup(jarj, i+1, kuup_jarj)
+        kuup_jarj.append(jarj[0] ** 3)
+        jarj.pop(0)
+
+        kuup_jarj += kuup(jarj)
 
     return kuup_jarj
 
 if __name__ == "__main__":
-    print(kuup([0, 1, 3]))
+    print(kuup([]))
